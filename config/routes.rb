d639b68c357d78e83eac47a8433d605069ee5640
Vamsi Mahesh <mahesh@digifutura.com>
2016-06-15 17:22:49 +0530

    Added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'home/hello'
+
+  root 'home#hello'
+
   mount API => '/'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
